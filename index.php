7d60f922bf97617a2d5d394d7a47f40bc2872eb4
Huan Azevedo <huancarlos89@gmail.com>
2021-09-18 18:50:57 -0300

    Formulário para consulta de CEP nacional, API ViaCep

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+//require "vendor/autoload.php";
+if($_GET['post'] == "ok"){
+require "Search.php";
+
+$busca = new Search();
+
+$resultado = $busca->getAdressFromZipcode($_POST['cep']);
+
+echo "<pre>";
+print_r($resultado);
+//echo 'Você mora na: '. $resultado['logradouro'].','. $resultado['bairro'];
+}
+echo '
+    <form action="exemplo.php?post=ok" method="POST" class="">
+    Informe seu CEP: <input type="text" name="cep"><input type="submit" value="Buscar">
+    </form>
+';
